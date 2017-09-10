045b50f0ea08668c0c2005561efe9349c77c36f1
dell <18514431885@163.com>
2017-09-10 22:58:27 +0800

    Initial commit

---
+++ app/src/main/java/com/yifeng/view/ui/BaseActivity.java
@@ -0,0 +1,19 @@
+package com.yifeng.view.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by YiFeng on 2017/7/23.
+ * Base Activity
+ */
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
---
+++ app/src/main/java/com/yifeng/view/ui/CircleLoadingActivity.java
@@ -0,0 +1,19 @@
+package com.yifeng.view.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.yifeng.view.R;
+
+/**
+ * Created by lxf on 2017/8/2.
+ */
+
+public class CircleLoadingActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle_loading);
+    }
+}
